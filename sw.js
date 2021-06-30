3d9524b84e8c19b3e1eccea69692b3e1aaa3f5ff
akmaka <akemi@sunmesse.co.jp>
2021-06-30 16:23:35 +0900

    v1

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // console.log('Hello from sw.js');
 
 // Service Worker のバージョンとキャッシュする App Shell を定義する
-var CACHE_NAME = 'pwa2-beta';
+var CACHE_NAME = 'pwa2-v1';
 var appShellFiles = [
   '/pwa-sample/',
   '/pwa-sample/common/js/main.js',
@@ -20,36 +20,58 @@ self.addEventListener('install', function(event) {
   );
 });
 
-// リクエストされたファイルが Service Worker にキャッシュされている場合
-// キャッシュからレスポンスを返す
+// リクエストをキャッシュして返す
+// https://developers.google.com/web/fundamentals/primers/service-workers?hl=ja
 self.addEventListener('fetch', function(event) {
-  // console.log('[Service Worker] Fetched resource '+event.request.url);
-  if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') return;
-
   event.respondWith(
-    caches.match(event.request).then(function (r) {
-      return r || fetch(event.request).then(function(response) {
-        return caches.open(CACHE_NAME).then(function(cache) {
-          console.log('[Service Worker] Caching new resource: '+event.request.url);
-          cache.put(event.request, response.clone());
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
           return response;
-        });
-      });
-    })
-  );
+        }
+
+        // IMPORTANT:Clone the request. A request is a stream and
+        // can only be consumed once. Since we are consuming this
+        // once by cache and once by the browser for fetch, we need
+        // to clone the response.
+        var fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // IMPORTANT:Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
 });
 
 // Cache Storage にキャッシュされているサービスワーカーのkeyに変更があった場合
 // 新バージョンをインストール後、旧バージョンのキャッシュを削除する
 self.addEventListener('activate', function(event) {
   event.waitUntil(
-    caches.keys().then(function (keyList) {
-        return Promise.all(keyList.map(function (key) {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
+    caches.keys().then(function (keysList) {
+      return Promise.all(keysList.map(function (key) {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }));
     })
     .then(function () {
       console.log('[ServiceWorker] ' + CACHE_NAME + ' activated');
