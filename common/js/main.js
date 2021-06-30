47d750ba8bd86520809a8fe5387e900492aac94e
akmaka <akemi@sunmesse.co.jp>
2021-06-30 12:02:50 +0900

    bug fix

--- common/js/main.js
+++ common/js/main.js
@@ -2,13 +2,13 @@
 if ('serviceWorker' in navigator) {
 	navigator.serviceWorker.register('/pwa-sample/common/js/sw.js').then(function(reg) {
 
-		if(reg.installing) {
-			console.log('Service worker installing');
-		} else if(reg.waiting) {
-			console.log('Service worker installed');
-		} else if(reg.active) {
-			console.log('Service worker active. Scope is ' + reg.scope);
-		}
+		// if(reg.installing) {
+		// 	console.log('Service worker installing');
+		// } else if(reg.waiting) {
+		// 	console.log('Service worker installed');
+		// } else if(reg.active) {
+		// 	console.log('Service worker active. Scope is ' + reg.scope);
+		// }
 
 	}).catch(function(error) {
 		// registration failed
--- common/js/sw.js
+++ common/js/sw.js
@@ -1,21 +1,24 @@
 // service-worker.js
+// console.log('Hello from sw.js');
 
 // Service Worker のバージョンとキャッシュする App Shell を定義する
-var NAME = 'v1-';
-var VERSION = '210625';
-var CACHE_NAME = NAME + VERSION;
-var urlsToCache = [
+var NAME = 'pwa';
+var VERSION = '-v3';
+var cacheName = NAME + VERSION;
+var appShellFiles = [
   '/pwa-sample/index.html',
   '/pwa-sample/common/js/main.js',
-  '/pwa-sample/common/css/main.css'
+  '/pwa-sample/common/css/main.css',
+  '/pwa-sample/img/01.png'
 ];
 
 // Service Worker へファイルをインストール
 self.addEventListener('install', function(event) {
+  console.log('[Service Worker] Install');
   event.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
+    caches.open(cacheName).then(function (cache) {
       console.log('[Service Worker] Caching all: app shell and content');
-      return cache.addAll(urlsToCache);
+      return cache.addAll(appShellFiles);
     })
   );
 });
@@ -23,12 +26,14 @@ self.addEventListener('install', function(event) {
 // リクエストされたファイルが Service Worker にキャッシュされている場合
 // キャッシュからレスポンスを返す
 self.addEventListener('fetch', function(event) {
-  if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') return;
+  // console.log('[Service Worker] Fetched resource '+event.request.url);
+  // if (event.request.cache === 'only-if-cached' && event.request.mode !== 'same-origin') return;
 
   event.respondWith(
-    caches.match(event.request).then(function (resp) {
-      return resp || fetch(event.request).then(function(response) {
-        return caches.open(CACHE_NAME).then(function(cache) {
+    caches.match(event.request).then(function (r) {
+      return r || fetch(event.request).then(function(response) {
+        return caches.open(cacheName).then(function(cache) {
+          console.log('[Service Worker] Caching new resource: '+event.request.url);
           cache.put(event.request, response.clone());
           return response;
         });
@@ -43,14 +48,14 @@ self.addEventListener('activate', function(event) {
   event.waitUntil(
     caches.keys().then(function (keyList) {
         return Promise.all(keyList.map(function (key) {
-          if (!CACHE_NAME.includes(key)) {
+          if (key !== cacheName) {
             return caches.delete(key);
           }
         })
       );
     })
     .then(function () {
-      console.log('[ServiceWorker] ' + CACHE_NAME + ' activated');
+      console.log('[ServiceWorker] ' + cacheName + ' activated');
     })
   );
 });
